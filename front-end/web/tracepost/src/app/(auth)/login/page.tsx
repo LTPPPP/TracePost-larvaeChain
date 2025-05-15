d6c329a73a01767052b22781c1a9e270960159ad
KHOA <aohkne@gmail.com>
2025-05-15 10:08:04 +0700

    Ui for login + register

--- front-end/web/tracepost/src/app/(auth)/login/page.tsx
+++ front-end/web/tracepost/src/app/(auth)/login/page.tsx
@@ -16,7 +16,7 @@ function Login() {
         height={150}
       />
 
-      <div className={cx('left-side', 'w-1/2')}>
+      <div className={cx('left-side', 'w-full', 'lg:w-1/2')}>
         <Link className={cx('logo')} href={'/'}>
           <Image src={'/img/logo.png'} alt='logo' width={50} height={50} />
         </Link>
@@ -38,12 +38,51 @@ function Login() {
           </div>
 
           <div className={cx('action')}>
-            First time here? <Link href={'register'}>Create one</Link>
+            First time here? <Link href={'/register'}>Create one</Link>
           </div>
         </div>
       </div>
 
-      <div className={cx('right-side', 'w-1/2')}></div>
+      <div className={cx('right-side', 'w-full', 'lg:w-1/2')}>
+        <div className={cx('right-container')}>
+          <h2 className={cx('form-title')}>Sign In</h2>
+
+          <form className={cx('login-form')}>
+            <div className={cx('form-group')}>
+              <label htmlFor='email' className={cx('form-label')}>
+                Email
+              </label>
+              <input type='email' id='email' className={cx('form-input')} placeholder='Email' />
+            </div>
+
+            <div className={cx('form-group')}>
+              <label htmlFor='password' className={cx('form-label')}>
+                Password
+              </label>
+              <input type='password' id='password' className={cx('form-input')} placeholder='Password' />
+              <div className={cx('show-password')}>
+                <input type='checkbox' id='showPassword' className={cx('checkbox')} />
+                <label htmlFor='showPassword' className={cx('checkbox-label')}>
+                  Show password
+                </label>
+              </div>
+            </div>
+
+            <button type='submit' className={cx('sign-in-btn')}>
+              SIGN IN
+            </button>
+
+            <div className={cx('divider')}>
+              <span>or</span>
+            </div>
+
+            <button type='button' className={cx('google-btn')}>
+              <Image src='/img/auth/google-icon.png' alt='Google' width={24} height={24} />
+              Continue with Google
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
