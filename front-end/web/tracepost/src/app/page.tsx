ec7c0064a59d0767a62b24204b8b102bfff2b4c4
KHOA <aohkne@gmail.com>
2025-05-17 12:49:45 +0700

    Done Home (Guide section is not good)

--- front-end/web/tracepost/src/app/page.tsx
+++ front-end/web/tracepost/src/app/page.tsx
@@ -5,21 +5,78 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import Typed from 'typed.js';
-import { FileText, BookOpen, Smartphone, MoveRight } from 'lucide-react';
+import { FileText, BookOpen, Smartphone, MoveRight, YoutubeIcon, GithubIcon, Mail } from 'lucide-react';
 
 import styles from './Home.module.scss';
 import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
-function Home() {
-  const [activeGuide, setActiveGuide] = useState('document');
-  const guideRefs = {
-    document: useRef(null),
-    guide: useRef(null),
-    app: useRef(null)
-  };
-  const typedTextRef = useRef(null);
+type ImagePosition = Partial<Record<'top' | 'bottom' | 'left' | 'right' | 'transform', string>>;
 
+interface ImageItem {
+  id: number;
+  src: string;
+  alt: string;
+  position: ImagePosition;
+}
+
+type GuideKey = 'document' | 'guide' | 'app';
+
+interface DivWithHandlers extends HTMLDivElement {
+  _moveHandler?: (e: MouseEvent) => void;
+  _leaveHandler?: () => void;
+}
+
+function Home() {
+  // HERO
+  const typedTextRef = useRef<HTMLSpanElement | null>(null);
+
+  // GUIDE
+  const [activeGuide, setActiveGuide] = useState<GuideKey>('document');
+
+  const guideRefs: Record<GuideKey, React.RefObject<HTMLDivElement | null>> = {
+    document: useRef<HTMLDivElement>(null),
+    guide: useRef<HTMLDivElement>(null),
+    app: useRef<HTMLDivElement>(null)
+  };
+
+  // ACHIVIE
+  const itemsRef = useRef<(HTMLDivElement | null)[]>([]);
+
+  const images: ImageItem[] = [
+    {
+      id: 1,
+      src: '/img/home/achivie1.png',
+      alt: 'Achievement 1',
+      position: { top: '120px', left: '80px' }
+    },
+    {
+      id: 2,
+      src: '/img/home/achivie1.png',
+      alt: 'Achievement 2',
+      position: { top: '40px', left: '40%' }
+    },
+    {
+      id: 3,
+      src: '/img/home/achivie1.png',
+      alt: 'Achievement 3',
+      position: { top: '120px', right: '80px' }
+    },
+    {
+      id: 4,
+      src: '/img/home/achivie1.png',
+      alt: 'Achievement 4',
+      position: { bottom: '50px', left: '200px' }
+    },
+    {
+      id: 5,
+      src: '/img/home/achivie1.png',
+      alt: 'Achievement 5',
+      position: { bottom: '50px', right: '200px' }
+    }
+  ];
+
+  // GUIDE
   useEffect(() => {
     const typed = new Typed(typedTextRef.current, {
       strings: [
@@ -57,29 +114,75 @@ function Home() {
       }
     };
 
-    // Check ngay khi component mount
     checkVisibility();
 
-    // Add listener cho scroll event
     window.addEventListener('scroll', checkVisibility);
 
-    // Cleanup function
     return () => {
       window.removeEventListener('scroll', checkVisibility);
     };
   }, []);
 
-  const handleGuideClick = (guide: any) => {
+  const handleGuideClick = (guide: GuideKey) => {
     setActiveGuide(guide);
-
-    // scroll functionality
-    // if (guideRefs[guide].current) {
-    //   guideRefs[guide].current.scrollIntoView({
-    //     behavior: 'smooth',
-    //     block: 'start'
-    //   });
-    // }
   };
+
+  // ACHIVIE
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent, item: HTMLDivElement): void => {
+      const rect = item.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+
+      const xPercent = x / rect.width - 0.5;
+      const yPercent = y / rect.height - 0.5;
+
+      const tiltX = yPercent * 10;
+      const tiltY = xPercent * -10;
+
+      item.style.animation = 'none';
+      item.style.transform = `perspective(1000px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale(1.1)`;
+    };
+
+    const handleMouseLeave = (item: HTMLDivElement): void => {
+      item.style.animation = '';
+      item.style.transform = '';
+    };
+
+    const items = itemsRef.current;
+
+    if (items && items.length > 0) {
+      items.forEach((item) => {
+        if (item) {
+          const itemWithHandlers = item as DivWithHandlers;
+
+          const moveHandler = (e: MouseEvent) => handleMouseMove(e, item);
+          const leaveHandler = () => handleMouseLeave(item);
+
+          item.addEventListener('mousemove', moveHandler);
+          item.addEventListener('mouseleave', leaveHandler);
+
+          // Gán handler vào DOM element (dạng mở rộng)
+          itemWithHandlers._moveHandler = moveHandler;
+          itemWithHandlers._leaveHandler = leaveHandler;
+        }
+      });
+    }
+
+    return () => {
+      if (items && items.length > 0) {
+        items.forEach((item) => {
+          if (item) {
+            const itemWithHandlers = item as DivWithHandlers;
+
+            if (itemWithHandlers._moveHandler) item.removeEventListener('mousemove', itemWithHandlers._moveHandler);
+            if (itemWithHandlers._leaveHandler) item.removeEventListener('mouseleave', itemWithHandlers._leaveHandler);
+          }
+        });
+      }
+    };
+  }, []);
 
   return (
     <div className={cx('wrapper')}>
@@ -274,6 +377,132 @@ function Home() {
 
         {/* --- */}
       </section>
+
+      {/* ENTERPRISE */}
+      <section className={cx('enterprise')} id='enterprise'>
+        <div className={cx('horizontal-line')} />
+
+        <div className={cx('enterprise-title')}>
+          <div className={cx('vertical-line')} />
+          Enterprise <span>Partners</span>
+        </div>
+
+        <div className={cx('enterprises-list')}>
+          <div className={cx('enterprises-item', 'fpt')}>
+            <Image src='/img/home/fpt.png' alt='fpt' width={60} height={10} />
+            <div className={cx('enterprises-name')}>FPT UNIVERSITY</div>
+          </div>
+
+          <div className={cx('enterprises-item', 'ctu')}>
+            <Image src='/img/home/ctu.png' alt='ctu' width={45} height={45} />
+            <div className={cx('enterprises-name')}>CAN THO UNIVERSITY</div>
+          </div>
+
+          <div className={cx('enterprises-item', 'fpt')}>
+            <Image src='/img/home/fpt.png' alt='fpt' width={60} height={10} />
+            <div className={cx('enterprises-name')}>FPT UNIVERSITY</div>
+          </div>
+
+          <div className={cx('enterprises-item', 'ctu')}>
+            <Image src='/img/home/ctu.png' alt='ctu' width={45} height={45} />
+            <div className={cx('enterprises-name')}>CAN THO UNIVERSITY</div>
+          </div>
+          <div className={cx('enterprises-item', 'fpt')}>
+            <Image src='/img/home/fpt.png' alt='fpt' width={60} height={10} />
+            <div className={cx('enterprises-name')}>FPT UNIVERSITY</div>
+          </div>
+
+          <div className={cx('enterprises-item', 'ctu')}>
+            <Image src='/img/home/ctu.png' alt='ctu' width={45} height={45} />
+            <div className={cx('enterprises-name')}>CAN THO UNIVERSITY</div>
+          </div>
+
+          <div className={cx('enterprises-item', 'fpt')}>
+            <Image src='/img/home/fpt.png' alt='fpt' width={60} height={10} />
+            <div className={cx('enterprises-name')}>FPT UNIVERSITY</div>
+          </div>
+
+          <div className={cx('enterprises-item', 'ctu')}>
+            <Image src='/img/home/ctu.png' alt='ctu' width={45} height={45} />
+            <div className={cx('enterprises-name')}>CAN THO UNIVERSITY</div>
+          </div>
+        </div>
+
+        <div className={cx('horizontal-line', 'bottom-line')} />
+
+        {/* ACHIVIE */}
+        <div className={cx('achive')}>
+          {/* Title in center */}
+          <h2 className={cx('achive__title')}>OUR ACHIVIE</h2>
+
+          {/* Images with positioning to match the layout */}
+          {images.map((image, index) => (
+            <div
+              key={image.id}
+              ref={(el) => {
+                itemsRef.current[index] = el;
+              }}
+              className={cx('achive__item')}
+              style={{
+                ...image.position
+              }}
+            >
+              <Image src={image.src} alt={image.alt} width={350} height={260} className={cx('achive__image')} />
+            </div>
+          ))}
+        </div>
+      </section>
+
+      {/* FOOTER */}
+      <footer>
+        <Image src={'/img/home/footer_logo.png'} alt='logo' width={1048} height={220} />
+
+        <div className={cx('divider')} />
+
+        <div className={cx('footer-container')}>
+          <div className={cx('footer-info')}>
+            <div className={cx('footer-description')}>2025 Fun is 9h. All Rights Reserved</div>
+            <div className={cx('footer-social')}>
+              <Link href={'#'}>
+                <GithubIcon size={25} />
+              </Link>
+
+              <Link href={'#'}>
+                <YoutubeIcon size={30} />
+              </Link>
+
+              <Link href={'mailto:'}>
+                <Mail size={25} />
+              </Link>
+            </div>
+          </div>
+
+          <div className={cx('footer-content')}>
+            <div className={cx('footer-navi')}>
+              <div className={cx('navi-title')}>NAVIGATION</div>
+              <div className={cx('navi-list')}>
+                <Link href={'#home'} className={cx('navi-item')}>
+                  HOME
+                </Link>
+
+                <Link href={'#features'} className={cx('navi-item')}>
+                  FEATURES
+                </Link>
+
+                <Link href={'#enterprise'} className={cx('navi-item')}>
+                  ENTERPRISE
+                </Link>
+              </div>
+            </div>
+
+            <div className={cx('footer-resources')}>
+              <Link href={'#'} className={cx('resources-title')}>
+                RESOURCES
+              </Link>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
